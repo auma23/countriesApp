0e56557bf42372f0457cecce312c063beef13f22
auma23 <dianaauma75@gmail.com>
2024-05-18 22:29:02 +0300

    adding routes and data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useContext } from 'react';
 import { DarkModeContext } from './Components/DarkModeContext';
 import Header from './Components/Header';
-import Search from './Components/Search';
 import Countries from './Components/Countries';
+import CountryDetail from './Components/CountryDetail';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
@@ -10,10 +11,39 @@ function App() {
   return (
     <div className={darkMode ? 'dark bg-black' : 'bg-gray-100 m-6'}>
       <Header />
-      <Search />
-      <Countries />
+      <Routes>
+        <Route path='/' element={<Countries />}/>
+        <Route path='/countryDetail' element={<CountryDetail />}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+// import { useContext } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { DarkModeContext } from './Components/DarkModeContext';
+// import Header from './Components/Header';
+// import Search from './Components/Search';
+// import Countries from './Components/Countries';
+// import CountryDetail from './Components/CountryDetail';
+
+// function App() {
+//   const { darkMode } = useContext(DarkModeContext);
+
+//   return (
+//     <div className={darkMode ? 'dark bg-black' : 'bg-gray-100 m-6'}>
+//       <Header />
+//       <Search />
+//       <Router>
+//         <Switch>
+//           <Route exact path="/" component={Countries} />
+//           <Route path="/country/:name" component={CountryDetail} />
+//         </Switch>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { DarkModeProvider } from './Components/DarkModeContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DarkModeProvider>
-      <App />
-    </DarkModeProvider>
+    
+      
+        <DarkModeProvider>
+         <BrowserRouter>
+           <App />
+          </BrowserRouter> 
+        </DarkModeProvider>     
+        
+    
   </React.StrictMode>,
   
 )
